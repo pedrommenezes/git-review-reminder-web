17e7c30c4d3654d0ea63cf06ecc33455479adc75
Pedro Menezes <pedro.menezes@kununu.com>
2018-11-19 11:39:30 +0000

    Updates

---
+++ precache-manifest.f28934c322bff8e5faffe04e6747b6d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18562ed8b78b73618863",
+    "url": "/git-review-reminder-web/static/js/runtime~main.18562ed8.js"
+  },
+  {
+    "revision": "3a03e968a6e88b2154fc",
+    "url": "/git-review-reminder-web/static/js/main.3a03e968.chunk.js"
+  },
+  {
+    "revision": "e64ce65a86b6863af711",
+    "url": "/git-review-reminder-web/static/js/1.e64ce65a.chunk.js"
+  },
+  {
+    "revision": "3a03e968a6e88b2154fc",
+    "url": "/git-review-reminder-web/static/css/main.82a07cd0.chunk.css"
+  },
+  {
+    "revision": "e64ce65a86b6863af711",
+    "url": "/git-review-reminder-web/static/css/1.f82199a3.chunk.css"
+  },
+  {
+    "revision": "35080f2827c619c7094a50c721adbc6f",
+    "url": "/git-review-reminder-web/index.html"
+  }
+];
